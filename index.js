682a73925e76668fec526eefb1f956b429b02d8b
Donna Engelberger <dengelberger@gmail.com>
2020-09-22 18:12:20 -0400

    It actually recorded responses in a .json file!

--- index.js
+++ index.js
@@ -4,20 +4,22 @@ var inquirer = require("inquirer");
 var fs = require('fs');
 
 // array of questions for user
-const questions = 
-    { nameQuest: "What is your name?",
-     titleQuest: "What is the title of your project?",
-     descriptionQuest: "Please describe your project.",
-     installQuest: "Please enter any instructions for installing your program.",
-     usageQuest: "How will your program be used?",
-     contribQuest: "Please provide guidelines for others to contribute to your project.",
-     testQuest: "Please provide instructions for testing your project.",
+const questions =
+{
+    nameQuest: "What is your name?",
+    titleQuest: "What is the title of your project?",
+    descriptionQuest: "Please describe your project.",
+    installQuest: "Please enter any instructions for installing your program.",
+    usageQuest: "How will your program be used?",
+    contribQuest: "Please provide guidelines for others to contribute to your project.",
+    testQuest: "Please provide instructions for testing your project.",
     licenseQuest: "Please choose a license from the list.",
-     gitHubQuest: "Please enter your GitHub username.",
-     emailQuest: "Please enter your email address." 
+    gitHubQuest: "Please enter your GitHub username.",
+    emailQuest: "Please enter your email address."
 };
 
-// function to write README file
+// function to write README file - see below at the end of the inquirer prompt for the creation of a .jsonf file.
+
 function writeToFile(fileName, data) {
 }
 
@@ -30,6 +32,9 @@ function init() {
 init();
 
 // inquirer prompt array drawing from questions array, above
+
+//  HOW DO I ADD SOME INFORMATIONAL PROMPT IN BETWEEN? TO USE EDITOR TYPE THE USER HAS TO KNOW HOW TO GET OUT OF THE EDITOR WHEN FINISHED.
+
 inquirer.prompt([
     {
         type: "input",
@@ -82,4 +87,17 @@ inquirer.prompt([
         name: "email",
         message: questions.emailQuest
     }
-]);
+]).then(function (data) {
+
+    var filename = data.name.toLowerCase().split(' ').join('') + ".json";
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), function (err) {
+
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Success!");
+    })
+});
+
